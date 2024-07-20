20f88cb8f91c0ba2728676dd05441c006bebbbdb
winnie279 <57951904+Yuji-207@users.noreply.github.com>
2024-07-20 20:58:54 +0900

    feat: Implement SAVE with file upload and description generation

--- app/projects/new/_component/NewProjectForm.tsx
+++ app/projects/new/_component/NewProjectForm.tsx
@@ -1,22 +1,18 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
-import TextArea from './TextArea'
+import NewProjectFormButton from './NewProjectFormButton'
 import TextInput from './TextInput'
+import { action } from '../action'
 
-const NewProjectForm = ({
-  action,
-}: {
-  action:(formData: FormData) => Promise<void>,
-}) => {
+const NewProjectForm = () => {
   const router = useRouter()
+
   return (
     <form
-      action={(formData: FormData) => {
-        action(formData)
-        router.push('/projects')
-      }}
+      action={action}
       className="flex flex-col absolute z-10 top-0 right-0 bg-gray-800 bg-opacity-80 max-h-[85%] max-w-[40%] overflow-auto text-white rounded-xl border-2 border-gray-400 m-5 mt-20 p-5"
+      encType="multipart/form-data"
     >
       <button onClick={router.back} className="flex justify-end">
         <svg
@@ -34,31 +30,12 @@ const NewProjectForm = ({
           />
         </svg>
       </button>
-      <h2 className="text-xl mb-5">
-        ご自身の業務や参加コミュニティでプロジェクトを遂行している際、壁にぶつかったと感じた体験談を教えてください。
-      </h2>
       <TextInput id="name">プロジェクト名*</TextInput>
-      <TextArea id="question1">
-        それはどんなプロジェクトでしたか？<br />お答えいただける範囲でお答えください。
-      </TextArea>
-      <TextArea id="question2">
-        「壁にぶつかった」と感じたのはどんな状況でしたか？
-      </TextArea>
-      <TextArea id="question3">
-        あなたはそのプロジェクトで壁にぶつかった時、<br />どのような行動をとりましたか？
-      </TextArea>
-      <TextArea id="question4">
-        そのプロジェクトはその後どうなりましたか？
-      </TextArea>
+      <input type="file" name="files" multiple className="my-3" />
       <p className="text-sm my-3">
         ※ 以下で送信いただいた内容は(株)会津の暮らし研究室が行うSAVEPOINT実装に向けた実証実験等に活用されます。クライアント名などの固有名詞や、個人を特定できる内容は記載しないようお願いいたします。
       </p>
-
-      <button
-        className="w-full text-gray-800 bg-white border border-gray-300 rounded-full my-3 p-3 hover:bg-gray-200 focus:outline-none focus:border-gray-600"
-      >
-        セーブする
-      </button>
+      <NewProjectFormButton />
     </form>
   )
 }
--- app/projects/new/action.ts
+++ app/projects/new/action.ts
@@ -1,55 +1,139 @@
 'use server'
 
+import path from 'path'
 // import { revalidateTag } from 'next/cache'
-import { redirect } from 'next/navigation'
-import fs from 'fs'
+// import { redirect } from 'next/navigation'
 import OpenAI from 'openai'
 import pgvector from 'pgvector'
 import { v4 as uuidv4 } from 'uuid'
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
 import { fromCognitoIdentityPool } from '@aws-sdk/credential-providers'
 import { PrismaClient } from '@prisma/client'
-import { ProjectCreate } from '@/app/_types/project'
+import { textMimeTypeList } from '@/app/_types/file'
+
+const s3Client = new S3Client({
+  region: process.env.AWS_BUCKET_REGION,
+  credentials: fromCognitoIdentityPool({
+    clientConfig: { region: process.env.AWS_BUCKET_REGION },
+    identityPoolId: process.env.AWS_IDENTITY_POOL_ID as string,
+  }),
+})
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+
+const PROMPT = `
+共有された **すべてのファイルと画像について** 、内容を以下の形式にまとめて出力してください。
+
+\`\`\`
+## 概要
+
+{ プロジェクトの概要を記入する。 }
+
+## 背景
+
+{ プロジェクトの背景を記入する。 }
+
+## 目的
+
+{ プロジェクトの目的を記入する。 }
+
+## 解決策
+
+{ プロジェクトの解決策を記入する。 }
+\`\`\`
+`
+
+const prisma = new PrismaClient()
 
 export const action = async (formData: FormData) => {
   // フォームの取得
   const name = formData.get('name')?.toString() ?? ''
-  const question1 = formData.get('question1')?.toString() ?? ''
-  const question2 = formData.get('question2')?.toString() ?? ''
-  const question3 = formData.get('question3')?.toString() ?? ''
-  const question4 = formData.get('question4')?.toString() ?? ''
+  const files = formData.getAll('files') as File[]
 
   // TODO: バリデーション
 
-  // ペイロード
+  try {
+    // Visionから入力するファイルのリストを作成
+    const imageContents = await Promise.all(
+      files
+      .filter((file) => file.type.startsWith('image/'))
+      .map(async (formFile) => {
+        const file = await openai.files.create({
+          file: formFile,
+          purpose: 'assistants',
+        });
+        
+        return {
+          type: 'image_file',
+          image_file: { file_id: file.id }
+        };
+      })
+    )
+
+    // File Searchから入力するファイルのリストを作成
+    const attachments = await Promise.all(
+      files
+      .filter((file) => textMimeTypeList.includes(path.extname(file.name)))
+      .map(async (formFile) => {
+        const file = await openai.files.create({
+          file: formFile,
+          purpose: 'assistants',
+        })
+
+        return {
+          file_id: file.id,
+          tools: [{ type: 'file_search' }]
+        }
+      })
+    )
+
+    const assistant = await openai.beta.assistants.create({
+      name: 'SAVE ASSISTANT',
+      description: '',
+      model: 'gpt-4o-mini',
+      tools: [{ type: 'file_search' }],
+    })
+
+    const thread = await openai.beta.threads.create({
+      // @ts-ignore
+      messages: [
+        {
+          'role': 'user',
+          'content': [
+            {
+              'type': 'text',
+              'text': PROMPT,
+            },
+            ...imageContents,
+          ],
+          attachments: attachments,
+        }
+      ]
+    })
+
+    const stream = openai.beta.threads.runs.stream(thread.id, {
+      assistant_id: assistant.id
+    })
+
+    let message = ''
+    for await (const event of stream) {
+      // @ts-ignore
+      const delta = event.data?.delta?.content?.[0]?.text?.value ?? ''
+      message += delta
+      process.stdout.write(delta)
+    }
+
+      // ペイロード
   const id = uuidv4()
   const teamId = 'admin'
   const description = `# ${name}
 
-## それはどんなプロジェクトでしたか？お答えいただける範囲でお答えください。
-
-${question1}
-
-## 「壁にぶつかった」と感じたのはどんな状況でしたか？
-
-${question2}
-
-## あなたはそのプロジェクトで壁にぶつかった時、どのような行動をとりましたか？
-
-${question3}
-
-## そのプロジェクトはその後どうなりましたか？
-
-${question4}
+${message}
 `
 
-  // リクエスト
-  const project: ProjectCreate = { id, name, description, teamId }
-  const prisma = new PrismaClient();
-
-  try {
     // エンべディング
-    const openai = new OpenAI();
     const response = await openai.embeddings.create({
       input: description,
       model: 'text-embedding-ada-002',
@@ -58,7 +142,7 @@ ${question4}
     // インサート
     const embedding = pgvector.toSql(response.data[0].embedding)
     const result: number = await prisma.$executeRaw`
-INSERT INTO 
+INSERT INTO
   public."Project" (
     id,
     name,
@@ -74,44 +158,19 @@ INSERT INTO
   )
 `
 
-    // 一時ファイルを作成
-    const dirKey = `projects/${id}/`
-    const fileKey = `${dirKey}README.md`
-    fs.writeFileSync('README.md', description);
-
-    // AWSのパラメータ
-    const bucketName = process.env.AWS_BUCKET_NAME_RAW;
-    const bucketRegion = process.env.AWS_BUCKET_REGION;
-    const identityPoolId = process.env.AWS_IDENTITY_POOL_ID;
-
-    const client = new S3Client({
-      region: bucketRegion,
-      credentials: fromCognitoIdentityPool({
-        clientConfig: { region: bucketRegion },
-        identityPoolId: identityPoolId as string,
-      }),
-    });
-
-    // ディレクトリの作成
-    const dirCommand = new PutObjectCommand({
-      Bucket: bucketName,
-      Key: dirKey,
-    })
-
-    await client.send(dirCommand);
-
     // ファイルのアップロード
-    const fileCommand = new PutObjectCommand({
-      Bucket: bucketName,
-      Key: fileKey,
-      Body: fs.readFileSync('README.md'),
-    })
+    const dirKey = `projects/${id}/`
+    for (const file of files) {
+      const fileBuffer = await file.arrayBuffer()
+      const fileKey = `${dirKey}${file.name}`
+      await s3Client.send(new PutObjectCommand({
+        Bucket: process.env.AWS_BUCKET_NAME_RAW,
+        Key: fileKey,
+        Body: Buffer.from(fileBuffer),
+      }))
+    }
 
-    await client.send(fileCommand)
-    
-    // revalidateTag('/projects')
-    redirect('/projects')
-
+    return message
   } catch (error) {
     console.log({error})
 
