695e389548872bebe4f36c54a7833685001812c2
kagawa0710 <58012645+kagawa0710@users.noreply.github.com>
2024-08-28 22:23:21 +0900

    add error handling

--- app/projects/(list)/_components/ProjectList.tsx
+++ app/projects/(list)/_components/ProjectList.tsx
@@ -1,20 +1,22 @@
-import { Project } from '@/app/_types/project'
-import Explore from './Explore'
-import { pca } from '../_lib/pca'
-import { loader } from '../loader'
+import { Project } from "@/app/_types/project";
+import Explore from "./Explore";
+import { pca } from "../_lib/pca";
+import { loader } from "../loader";
 
 const ProjectList = async () => {
-  const projects = await loader()
-  const embeddings = projects.map((project) => project.embedding)
-  const newEmbeddings = pca(embeddings)
-  const newProjects: Project[] = projects.map((project: any, i: number) => {
-    project.embedding = newEmbeddings[i].slice(0, 3)
-    return project
-  })
-  
-  return (
-    <Explore projects={newProjects} />
-  )
-}
+  const projects = await loader();
+  if (projects.length < 1) {
+    return <></>;
+  }
+  const embeddings = projects.map((project) => project.embedding);
+  const newEmbeddings = pca(embeddings);
 
-export default ProjectList
+  const newProjects: Project[] = projects.map((project: any, i: number) => {
+    project.embedding = newEmbeddings[i].slice(0, 3);
+    return project;
+  });
+
+  return <Explore projects={newProjects} />;
+};
+
+export default ProjectList;
