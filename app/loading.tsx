b99d2713359363862354176b0c305aa4d45dcdf3
kagawa0710 <58012645+kagawa0710@users.noreply.github.com>
2024-10-20 13:42:07 +0900

    add delete user function

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,22 +1,24 @@
-'use client'
+"use client";
 
-import React from 'react';
-import { LinearProgress, Typography, Box } from '@mui/material';
+import React from "react";
+import { LinearProgress, Typography, Box } from "@mui/material";
 
 export default function Loading() {
   return (
     <Box
       sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh',
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
       }}
       className="bg-black"
     >
-      <Typography variant="h5" className='text-white'>Welcome to SAVEPOINT</Typography>
-      <LinearProgress className="w-1/3 m-20"/>
+      <Typography variant="h5" className="text-white">
+        Welcome to SAVEPOINT
+      </Typography>
+      <LinearProgress className="w-1/3 m-20" />
     </Box>
-  )
+  );
 }
